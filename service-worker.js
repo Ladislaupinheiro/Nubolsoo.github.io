4e057bd31d3f5bb3e0d09f95bd591c30a78f19cd
ladislaupinheiro <66391372+Ladislaupinheiro@users.noreply.github.com>
2025-08-20 10:46:20 +0100

    Update 5 service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,18 @@
-const CACHE_NAME = "nubolso-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./styles.css", 
-  "./app.js"
-];
-
-// Instala o SW e adiciona ao cache
 self.addEventListener("install", event => {
+  console.log("Service Worker instalado");
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open("nubolsoo-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
     })
   );
 });
 
-// Ativa o SW e limpa caches antigos
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Responde com cache primeiro
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
